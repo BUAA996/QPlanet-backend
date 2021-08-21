2c4567b9c28c5da4b83cdaf2f4f4876b09c205a1
Sally <1915627075@qq.com>
2021-08-21 21:06:10 +0800

    feat:update questionaire sort

--- questionnaire/urls.py
+++ questionnaire/urls.py
@@ -2,11 +2,12 @@ from django.urls import path
 from questionnaire.views import *
 
 urlpatterns = [
-	path('create/', create),
-	path('list/', list),
-	path('reset/', reset_hash),
-	path('delete/', delete),
-	path('recover/', recover),
-	path('release/', release),
-	path('close/', close),
+	path('create', create),
+	path('list', list),
+	path('reset', reset_hash),
+	path('delete', delete),
+	path('recover', recover),
+	path('release', release),
+	path('close', close),
+	path('sorted', get_sorted_questionnaires),
 ]
